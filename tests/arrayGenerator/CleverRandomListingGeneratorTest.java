de4299f18fac84dcb3496427cb72e35d24b20285
Hugh Osborne <h.r.osborne@hud.ac.uk>
2020-09-24 17:19:55 +0100

    CleverRandomListingGeneratorTest updated to be subclass of RandomListingGeneratorTest

--- tests/arrayGenerator/CleverRandomListingGeneratorTest.java
+++ tests/arrayGenerator/CleverRandomListingGeneratorTest.java
@@ -2,7 +2,7 @@ package arrayGenerator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CleverRandomListingGeneratorTest {
+class CleverRandomListingGeneratorTest extends RandomListingGeneratorTest {
     protected ArrayGenerator getGenerator(int size) {
         return new CleverRandomListingGenerator(size); // make getGenerator return a CleverRandomListingGenerator
     }
